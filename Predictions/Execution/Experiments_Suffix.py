4f1766d7de3ec43ce1d3bf25053b44d54598291c
Nils Berni <30769791+NilsBerni@users.noreply.github.com>
2021-06-10 23:17:12 +0200

    commenting

--- Predictions/Execution/Experiments_Suffix.py
+++ Predictions/Execution/Experiments_Suffix.py
@@ -22,6 +22,69 @@ from Predictions.Execution.Experiments_Variables import EDBN, CAMARGO, DIMAURO, 
 from Predictions.Execution.Experiments_Variables import K_EDBN
 from Utils.LogFile import LogFile
 
+def run_suffix_pred(argv):
+    if len(argv) < 2:
+        print("Missing arguments, expected: METHOD and DATA")
+        return
+
+    method = argv[0]
+    data = argv[1]
+
+    train = [train for train in DATA_DESC if train["data"] == data][0]
+    dataset_folder = os.path.join(DATA_FOLDER, train["folder"])
+    model_folder = os.path.join(OUTPUT_FOLDER, str.lower(train["data"]), "models", str.lower(method))
+
+    edbn_k = K_EDBN
+
+    if method == CAMARGO:
+        if len(argv) < 3:
+            print("Please indicate the architecture to use: shared_cat or specialized")
+            return
+        else:
+            model_folder = os.path.join(model_folder, argv[2])
+    elif method == EDBN:
+        if len(argv) >= 3:
+            edbn_k = int(argv[2])
+        else:
+            edbn_k = None
+        model_folder = os.path.join(model_folder, str(edbn_k))
+
+    ###
+    # Register Start time
+    ###
+    start_time = time.mktime(time.localtime())
+    start_time_str = time.strftime("%d-%m-%y %H:%M:%S", time.localtime())
+    time_output = open(os.path.join(model_folder, "timings_suffix.log"), 'a')
+    time_output.write("Starting time: %s\n" % start_time_str)
+
+    ###
+    # Execute chosen method
+    ###
+    print("EXPERIMENT SUFFIX PREDICTION:", argv)
+    if method == EDBN:
+        test_edbn(dataset_folder, model_folder, edbn_k)
+    elif method == CAMARGO:
+        if len(argv) < 3:
+            print("Please indicate the architecture to use: shared_cat or specialized")
+            return
+        architecture = str.lower(argv[2])
+        test_camargo(dataset_folder, model_folder, architecture)
+    elif method == LIN:
+        test_lin(dataset_folder, model_folder)
+    elif method == DIMAURO:
+        if len(argv) == 3:
+            os.environ["CUDA_VISIBLE_DEVICES"] = argv[2]
+        test_dimauro(dataset_folder, model_folder)
+    elif method == TAX:
+        test_tax(dataset_folder, model_folder)
+
+    ###
+    # Register End time
+    ###
+    current_time = time.mktime(time.localtime())
+    current_time_str = time.strftime("%d-%m-%y %H:%M:%S", time.localtime())
+    time_output.write("End time: %s\n" % current_time_str)
+    time_output.write("Duration: %fs\n\n" % (current_time - start_time))
 
 def test_edbn(dataset_folder, model_folder, k=None):
     from Predictions.eDBN_Prediction import predict_suffix
@@ -105,70 +168,6 @@ def test_tax(dataset_folder, model_folder):
 
 def main(argv):
     run_suffix_pred(argv)
-
-def run_suffix_pred(argv):
-    if len(argv) < 2:
-        print("Missing arguments, expected: METHOD and DATA")
-        return
-
-    method = argv[0]
-    data = argv[1]
-
-    train = [train for train in DATA_DESC if train["data"] == data][0]
-    dataset_folder = os.path.join(DATA_FOLDER, train["folder"])
-    model_folder = os.path.join(OUTPUT_FOLDER, str.lower(train["data"]), "models", str.lower(method))
-
-    edbn_k = K_EDBN
-
-    if method == CAMARGO:
-        if len(argv) < 3:
-            print("Please indicate the architecture to use: shared_cat or specialized")
-            return
-        else:
-            model_folder = os.path.join(model_folder, argv[2])
-    elif method == EDBN:
-        if len(argv) >= 3:
-            edbn_k = int(argv[2])
-        else:
-            edbn_k = None
-        model_folder = os.path.join(model_folder, str(edbn_k))
-
-    ###
-    # Register Start time
-    ###
-    start_time = time.mktime(time.localtime())
-    start_time_str = time.strftime("%d-%m-%y %H:%M:%S", time.localtime())
-    time_output = open(os.path.join(model_folder, "timings_suffix.log"), 'a')
-    time_output.write("Starting time: %s\n" % start_time_str)
-
-    ###
-    # Execute chosen method
-    ###
-    print("EXPERIMENT SUFFIX PREDICTION:", argv)
-    if method == EDBN:
-        test_edbn(dataset_folder, model_folder, edbn_k)
-    elif method == CAMARGO:
-        if len(argv) < 3:
-            print("Please indicate the architecture to use: shared_cat or specialized")
-            return
-        architecture = str.lower(argv[2])
-        test_camargo(dataset_folder, model_folder, architecture)
-    elif method == LIN:
-        test_lin(dataset_folder, model_folder)
-    elif method == DIMAURO:
-        if len(argv) == 3:
-            os.environ["CUDA_VISIBLE_DEVICES"] = argv[2]
-        test_dimauro(dataset_folder, model_folder)
-    elif method == TAX:
-        test_tax(dataset_folder, model_folder)
-
-    ###
-    # Register End time
-    ###
-    current_time = time.mktime(time.localtime())
-    current_time_str = time.strftime("%d-%m-%y %H:%M:%S", time.localtime())
-    time_output.write("End time: %s\n" % current_time_str)
-    time_output.write("Duration: %fs\n\n" % (current_time - start_time))
 
 
 if __name__ == "__main__":
--- Predictions/Execution/Experiments_Train.py
+++ Predictions/Execution/Experiments_Train.py
@@ -26,6 +26,110 @@ from Predictions.Execution.Experiments_Variables import K_EDBN, DIMAURO_PARAMS
 #from Predictions import get_data
 from Utils.LogFile import LogFile
 
+def run_training(argv):
+    if len(argv) < 2:
+        print("Missing arguments, expected: METHOD and DATA")
+        return
+    else:
+        method = argv[0]
+        data = argv[1]
+
+    if not os.path.exists(DATA_FOLDER):
+        os.mkdir(DATA_FOLDER)
+
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.mkdir(OUTPUT_FOLDER)
+
+    # Check for all input data if already exist. Otherwise, create data
+    for train in DATA_DESC:
+        dataset_folder = os.path.join(DATA_FOLDER, train["folder"])
+        if not os.path.exists(dataset_folder):
+            os.mkdir(dataset_folder)
+
+            full_logfile, _ = get_data(train["data"], None, 2, False, False, False, False)
+            print(full_logfile.data)
+            if len(full_logfile.data.columns) == 4:
+                full_logfile.data.columns = ["case","event","role", "time"]
+            else:
+                full_logfile.data.columns = ["case","event","role"]
+            full_logfile.trace = "case"
+            full_logfile.activity = "event"
+            create_csv_file_header(full_logfile.data.to_dict('records'), os.path.join(dataset_folder,'full_log.csv'))
+
+            train_logfile, test_logfile = full_logfile.splitTrainTest(70)
+            create_csv_file_header(train_logfile.data.to_dict('records'), os.path.join(dataset_folder,'train_log.csv'))
+            create_csv_file_header(test_logfile.data.to_dict('records'), os.path.join(dataset_folder,'test_log.csv'))
+
+        output_folder = os.path.join(OUTPUT_FOLDER, train["folder"])
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+            os.mkdir(os.path.join(output_folder, "models"))
+
+    train = [train for train in DATA_DESC if train["data"] == data][0]
+    dataset_folder = os.path.join(DATA_FOLDER, train["folder"])
+    model_folder = os.path.join(OUTPUT_FOLDER, str.lower(train["data"]), "models", str.lower(method))
+
+    if not os.path.exists(model_folder):
+        os.mkdir(model_folder)
+
+    edbn_k = K_EDBN
+
+    if method == CAMARGO:
+        if len(argv) < 3:
+            print("Please indicate the architecture to use: shared_cat or specialized")
+            return
+        else:
+            model_folder = os.path.join(model_folder, argv[2])
+            if not os.path.exists(model_folder):
+                os.mkdir(model_folder)
+    elif method == EDBN:
+        if len(argv) >= 3:
+            edbn_k = int(argv[2])
+        else:
+            edbn_k = None
+        model_folder = os.path.join(model_folder, str(edbn_k))
+        if not os.path.exists(model_folder):
+            os.mkdir(model_folder)
+    ###
+    # Register Start time
+    ###
+    start_time = time.mktime(time.localtime())
+    start_time_str = time.strftime("%d-%m-%y %H:%M:%S", time.localtime())
+    time_output = open(os.path.join(model_folder, "timings_train.log"), 'a')
+    time_output.write("Starting time: %s\n" % start_time_str)
+
+    ###
+    # Execute chosen method
+    ###
+    print("EXPERIMENT TRAINING MODEL:", argv)
+    if method == EDBN:
+        if len(argv) == 4:
+            train_edbn(dataset_folder, model_folder, edbn_k, argv[3] == "next")
+        else:
+            train_edbn(dataset_folder, model_folder, edbn_k)
+    elif method == CAMARGO:
+        if len(argv) < 3:
+            print("Please indicate the architecture to use: shared_cat or specialized")
+            return
+        architecture = str.lower(argv[2])
+        train_camargo(dataset_folder, model_folder, architecture)
+    elif method == LIN:
+        train_lin(dataset_folder, model_folder)
+    elif method == DIMAURO:
+        if len(argv) == 3:
+            os.environ["CUDA_VISIBLE_DEVICES"] = argv[2]
+        train_dimauro(dataset_folder, model_folder, DIMAURO_PARAMS.get(data, None))
+    elif method == TAX:
+        train_tax(dataset_folder, model_folder)
+
+    ###
+    # Register End time
+    ###
+    current_time = time.mktime(time.localtime())
+    current_time_str = time.strftime("%d-%m-%y %H:%M:%S", time.localtime())
+    time_output.write("End time: %s\n" % current_time_str)
+    time_output.write("Duration: %fs\n\n" % (current_time - start_time))
+
 
 def train_edbn(data_folder, model_folder, k = None, next_event = True):
     from EDBN.Execute import train
@@ -157,110 +261,6 @@ def train_tax(data_folder, model_folder):
 
 def main(argv):
     run_training(argv)
-
-def run_training(argv):
-    if len(argv) < 2:
-        print("Missing arguments, expected: METHOD and DATA")
-        return
-    else:
-        method = argv[0]
-        data = argv[1]
-
-    if not os.path.exists(DATA_FOLDER):
-        os.mkdir(DATA_FOLDER)
-
-    if not os.path.exists(OUTPUT_FOLDER):
-        os.mkdir(OUTPUT_FOLDER)
-
-    # Check for all input data if already exist. Otherwise, create data
-    for train in DATA_DESC:
-        dataset_folder = os.path.join(DATA_FOLDER, train["folder"])
-        if not os.path.exists(dataset_folder):
-            os.mkdir(dataset_folder)
-
-            full_logfile, _ = get_data(train["data"], None, 2, False, False, False, False)
-            print(full_logfile.data)
-            if len(full_logfile.data.columns) == 4:
-                full_logfile.data.columns = ["case","event","role", "time"]
-            else:
-                full_logfile.data.columns = ["case","event","role"]
-            full_logfile.trace = "case"
-            full_logfile.activity = "event"
-            create_csv_file_header(full_logfile.data.to_dict('records'), os.path.join(dataset_folder,'full_log.csv'))
-
-            train_logfile, test_logfile = full_logfile.splitTrainTest(70)
-            create_csv_file_header(train_logfile.data.to_dict('records'), os.path.join(dataset_folder,'train_log.csv'))
-            create_csv_file_header(test_logfile.data.to_dict('records'), os.path.join(dataset_folder,'test_log.csv'))
-
-        output_folder = os.path.join(OUTPUT_FOLDER, train["folder"])
-        if not os.path.exists(output_folder):
-            os.mkdir(output_folder)
-            os.mkdir(os.path.join(output_folder, "models"))
-
-    train = [train for train in DATA_DESC if train["data"] == data][0]
-    dataset_folder = os.path.join(DATA_FOLDER, train["folder"])
-    model_folder = os.path.join(OUTPUT_FOLDER, str.lower(train["data"]), "models", str.lower(method))
-
-    if not os.path.exists(model_folder):
-        os.mkdir(model_folder)
-
-    edbn_k = K_EDBN
-
-    if method == CAMARGO:
-        if len(argv) < 3:
-            print("Please indicate the architecture to use: shared_cat or specialized")
-            return
-        else:
-            model_folder = os.path.join(model_folder, argv[2])
-            if not os.path.exists(model_folder):
-                os.mkdir(model_folder)
-    elif method == EDBN:
-        if len(argv) >= 3:
-            edbn_k = int(argv[2])
-        else:
-            edbn_k = None
-        model_folder = os.path.join(model_folder, str(edbn_k))
-        if not os.path.exists(model_folder):
-            os.mkdir(model_folder)
-    ###
-    # Register Start time
-    ###
-    start_time = time.mktime(time.localtime())
-    start_time_str = time.strftime("%d-%m-%y %H:%M:%S", time.localtime())
-    time_output = open(os.path.join(model_folder, "timings_train.log"), 'a')
-    time_output.write("Starting time: %s\n" % start_time_str)
-
-    ###
-    # Execute chosen method
-    ###
-    print("EXPERIMENT TRAINING MODEL:", argv)
-    if method == EDBN:
-        if len(argv) == 4:
-            train_edbn(dataset_folder, model_folder, edbn_k, argv[3] == "next")
-        else:
-            train_edbn(dataset_folder, model_folder, edbn_k)
-    elif method == CAMARGO:
-        if len(argv) < 3:
-            print("Please indicate the architecture to use: shared_cat or specialized")
-            return
-        architecture = str.lower(argv[2])
-        train_camargo(dataset_folder, model_folder, architecture)
-    elif method == LIN:
-        train_lin(dataset_folder, model_folder)
-    elif method == DIMAURO:
-        if len(argv) == 3:
-            os.environ["CUDA_VISIBLE_DEVICES"] = argv[2]
-        train_dimauro(dataset_folder, model_folder, DIMAURO_PARAMS.get(data, None))
-    elif method == TAX:
-        train_tax(dataset_folder, model_folder)
-
-    ###
-    # Register End time
-    ###
-    current_time = time.mktime(time.localtime())
-    current_time_str = time.strftime("%d-%m-%y %H:%M:%S", time.localtime())
-    time_output.write("End time: %s\n" % current_time_str)
-    time_output.write("Duration: %fs\n\n" % (current_time - start_time))
 
 
 if __name__ == "__main__":
